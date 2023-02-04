bfdce12f03f398fa81df8c0682eb153f49ad8b74
mangasteak <leo.maman@epitech.eu>
2023-02-04 15:26:03 +0100

    [ADD] | forgotten hpp

---
+++ ecs/systems/EventSystem.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** EventSystem
+*/
+
+#ifndef EVENTSYSTEM_HPP_
+#define EVENTSYSTEM_HPP_
+
+#include <SFML/Graphics.hpp>
+#include "../Entity.hpp"
+#include <memory>
+
+namespace rtype {
+    class EventSystem {
+        public:
+        
+            EventSystem(sf::RenderWindow& window);
+            ~EventSystem();
+            void update(std::vector<std::shared_ptr<rtype::Entity>>& entities);
+
+        protected:
+        private:
+            sf::RenderWindow& _window;
+};
+}
+
+#endif /* !EVENTSYSTEM_HPP_ */
