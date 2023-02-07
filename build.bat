ffdbb8dadb4aca0c4fa426849c2c63e3157c8a6a
Patate <arthur.duhot@epitech.eu>
2023-02-07 19:18:44 +0100

    [UPDATE] now vcpkg install works and I can install qt and sfml

--- build.bat
+++ build.bat
@@ -1,19 +1,11 @@
-    @echo off
+@echo off
 
-rem Install Chocolatey
-
-echo check if chocolatey is installed
-
-powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-. $profile
-choco -version
-
-choco install cmake --installargs 'ADD_CMAKE_TO_PATH=System'
-
+echo install vcpkg
 git clone https://github.com/Microsoft/vcpkg.git
 
-.\vcpkg\bootstrap-vcpkg.bat
+echo config vcpkg
+call .\vcpkg\bootstrap-vcpkg.bat
 
-.\vcpkg\vcpkg install sfml:x64-windows
-.\vcpkg\vcpkg install qt5-base
+echo install sfml
+.\\vcpkg\\vcpkg install sfml:x64-windows
+.\\vcpkg\\vcpkg install qt5-base
