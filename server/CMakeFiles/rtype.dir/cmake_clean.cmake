fe92139e42e64c2e0a3d03bb6426a7c4649ae362
loic <loic.rouzaud@epitech.eu>
2023-02-08 11:57:29 +0100

    [ADD] - Files that i need to keep

---
+++ server/CMakeFiles/rtype.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rtype_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/rtype_autogen.dir/ParseCache.txt"
+  "rtype_autogen"
+  "CMakeFiles/rtype.dir/rtype_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/rtype.dir/rtype_autogen/mocs_compilation.cpp.o.d"
+  "rtype"
+  "rtype.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rtype.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ server/CMakeFiles/rtype.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for rtype.
+# This may be replaced when dependencies are built.
