0e0d03b3d9461e5fda558f61113ac2cbc1317b6c
loic <loic.rouzaud@epitech.eu>
2023-02-13 12:18:23 +0100

    [FIX] position of files

---
+++ client/Archi/SceneManager.hpp
@@ -0,0 +1,40 @@
+/*
+** EPITECH PROJECT, 2023
+** sceneManager
+** File description:
+** SceneManager
+*/
+
+#ifndef SCENEMANAGER_HPP_
+#define SCENEMANAGER_HPP_
+
+#include <iostream>
+#include <map>
+#include "IScene.hpp"
+
+
+namespace rtype {
+class SceneManager {
+    public:
+        /**
+         * @brief Construct a new Scene Manager object
+         * 
+         */
+        SceneManager();
+
+        /**
+         * @brief Destroy the Scene Manager object
+         * 
+         */
+        ~SceneManager();
+
+    protected:
+        std::map<std::string, std::string> _scenes;
+        SceneType _currentScene;
+        bool _shouldClose;
+
+    private:
+};
+};
+
+#endif /* !SCENEMANAGER_HPP_ */
