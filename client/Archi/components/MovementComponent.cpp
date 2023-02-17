986ecc6a71753dcd2c3ce12d04fd359a2ef738e3
Saad Berrada <72012880+Codrux2200@users.noreply.github.com>
2023-02-17 11:59:30 +0100

    [FIX] compil with adding new empty cpp's

---
+++ client/Archi/components/MovementComponent.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** MovementComponent
+*/
+#include "../Include/components/MovementComponent.hpp"
+namespace rtype{
+MovementComponent::MovementComponent()
+    {
+    }
+
+    MovementComponent::~MovementComponent()
+    {
+    }
+}
