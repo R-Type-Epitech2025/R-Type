d25c165d0453d59b77058f836759dbca2a1d045e
Ilan Chekroun <ilan.chekroun@epitech.eu>
2023-02-25 11:14:06 +0100

    [EDIT] - Adding background and icon bundle

--- build.sh
+++ build.sh
@@ -12,8 +12,17 @@ COMPILED=false
 # CMake compilation
 cd client
 echo -e "${GREEN}Compilation du ${YELLOW}client${GREEN}...${WHITE}"
-cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake &> /dev/null
-cmake --build build -v &> /dev/null
+# mkdir build if not exist
+if [ ! -d "build" ]; then
+    mkdir build
+else
+    rm -rf build
+    mkdir build
+fi
+# cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake &> /dev/null
+# cmake --build build -v &> /dev/null
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
+cmake --build build -v
 echo -e "${GREEN}Compilation terminée.${WHITE}"
 
 # CPack Build for Windows on NSIS
@@ -38,7 +47,6 @@ if [ $OS == "Darwin" ] && [ $COMPILED == false ]; then
     echo -e "${GREEN}Build du ${YELLOW}client${GREEN}...${WHITE} pour ${YELLOW}MacOS${WHITE}"
     cd build
     cpack -G DragNDrop
-    hdiutil create -volname R-Type -srcfolder ${APP_NAME}.app -srcfolder assets -ov -format UDZO ${APP_NAME}.dmg
     echo -e "${GREEN}Build terminée.${WHITE}"
 fi
 
