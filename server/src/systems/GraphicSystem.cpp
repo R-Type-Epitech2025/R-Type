e2fdbb4393d9e9ad1036313897fcdbcbf96d5d15
mangasteak <leo.maman@epitech.eu>
2023-03-05 21:55:35 +0100

    [ADD] - in graphic update , delete entities that goes outside screen size

--- server/src/systems/GraphicSystem.cpp
+++ server/src/systems/GraphicSystem.cpp
@@ -19,9 +19,17 @@ namespace rtype {
     // GraphicSystem::~GraphicSystem()
     // {
     // }
-
-    void GraphicSystem::update(rtype::SceneManager* manager, uint64_t time)
+    void GraphicSystem::update(SceneManager* manager, uint64_t time)
     {
+        Scene *scene = manager->getCurrentScene();
+
+        for (auto &entity : scene->get_entities()) {
+            if (entity->container.graphic_component->position.x > (1920 + 200) 
+            || (entity->container.graphic_component->position.y > (1080 +200)) 
+            || (entity->container.graphic_component->position.x < (entity->container.graphic_component->size.width - 200)) 
+            || (entity->container.graphic_component->position.x < (entity->container.graphic_component->size.width - 200))) {
+                scene->removeEntity(entity);
+            }
     }
 
     void GraphicSystem::onUpdateSprites(std::vector<Entity *> entities)
